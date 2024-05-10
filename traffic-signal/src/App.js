cfee8c21b266a9b0f7beb034f626719288a97151
jyothiprakashk <jyothiprakash097@gmail.com>
2024-05-10 13:39:56 +0530

    prettier format changes

--- traffic-signal/src/App.js
+++ traffic-signal/src/App.js
@@ -1,46 +1,41 @@
-import './App.css';
-import React, { useEffect,useState } from 'react'
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
-const SIGNAL_CONFIG={
+const SIGNAL_CONFIG = {
   red: {
-    duration:6000,
-    next: "green"
+    duration: 6000,
+    next: "green",
   },
-  green:{
-    duration:5000,
-    next:"yellow"
+  green: {
+    duration: 5000,
+    next: "yellow",
   },
   yellow: {
     duration: 3000,
-    next:"red"
-  }
-}
+    next: "red",
+  },
+};
 
 function App() {
-  const [color,setColor]=useState("green")
+  const [color, setColor] = useState("green");
 
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setColor((prevColor) => {
+        let { next } = SIGNAL_CONFIG[prevColor];
+        return next;
+      });
+    }, SIGNAL_CONFIG[color].duration);
 
-  useEffect(()=>{
-    let timer=setInterval(()=>{
-      setColor((prevColor)=>{
-        let {next}=SIGNAL_CONFIG[prevColor]
-        return next
-      })
-    },SIGNAL_CONFIG[color].duration)
-
-    return()=>{
-      clearInterval(timer)
-    }
-  },[color])
-
-
+    return () => {
+      clearInterval(timer);
+    };
+  }, [color]);
 
   return (
     <div className="signalContainer">
-      {Object.keys(SIGNAL_CONFIG).map((each)=>{
-        return(
-            <Light currentColor={each===color?color:""}  />
-        )
+      {Object.keys(SIGNAL_CONFIG).map((each) => {
+        return <Light currentColor={each === color ? color : ""} />;
       })}
     </div>
   );
@@ -48,9 +43,8 @@ function App() {
 
 export default App;
 
-
-const Light=({currentColor})=>{
+const Light = ({ currentColor }) => {
   return (
-    <div style={{backgroundColor:currentColor}} className='signalLight'/>
-  )
-}
+    <div style={{ backgroundColor: currentColor }} className="signalLight" />
+  );
+};
